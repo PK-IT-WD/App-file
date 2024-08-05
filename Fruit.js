7df9b6ac1dfa72320d6d8c9fdf2301a51691ce02
PK-IT-WD <pradeepkumaritwd@gmail.com>
2024-08-05 17:03:01 +0530

    Added few lines of code

--- Fruit.js
+++ Fruit.js
@@ -8,3 +8,8 @@ const basket_heading = document.getElementById('basket-heading');
 basket_heading.style.color = 'green';
 const thanks = document.getElementById('thanks');
 thanks.innerHTML = '<p>Please visit us again</p>'
+const fruit = document.getElementsByClassName('fruit');
+fruit[2].style.backgroundColor = 'yellow';
+for(let i = 0; i < fruit.length; i++) {
+  fruit[i].style.fontWeight = 'bold';
+}
