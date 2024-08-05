d1d6ffaf19cad77923286b7d0542de73a8fec4fd
PK-IT-WD <pradeepkumaritwd@gmail.com>
2024-08-05 16:50:07 +0530

    Added fruit

---
+++ Fruit.js
@@ -0,0 +1,10 @@
+const main_heading = document.getElementById('main-heading');
+main_heading.textContent = 'Fruit World';
+main_heading.style.color = 'orange';
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom = 'orange';
+const basket_heading = document.getElementById('basket-heading');
+basket_heading.style.color = 'green';
+const thanks = document.getElementById('thanks');
+thanks.innerHTML = '<p>Please visit us again</p>'
