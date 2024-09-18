0a2893ed6edc123d41f1c94c9d5caf250537940d
PK-IT-WD <pradeepkumaritwd@gmail.com>
2024-09-18 11:51:22 +0530

    Product Page

--- formServer.js
+++ formServer.js
@@ -1,15 +1,21 @@
 const express = require('express');
-
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the Middleware');
-    next();
+app.use(express.urlencoded({extended: true}));
+
+app.use('/add-product', (req, res, next) => {
+    console.log('Add Product Page');
+    res.send('<form action="/product" method="POST"><h1>Product Name: </h1><input type="text" name="title"><br><br><h1>Size: </h1><input type="number" name="size"><button type="submit">Submit</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('In another Middleware');
-    res.send('Hello from Express');
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
+    console.log('Main Page');
+    res.send('<h1>Main Page<h1>');
 });
 
 app.listen(4000);
